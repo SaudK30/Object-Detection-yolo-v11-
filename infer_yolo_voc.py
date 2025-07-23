4aaaad59cebfea874e818e3f41a3982363c40347
SaudK30 <saud130khan@gmail.com>
2025-07-23 19:39:02 +0530

    Update infer_yolo_voc.py

--- infer_yolo_voc.py
+++ infer_yolo_voc.py
@@ -7,7 +7,7 @@ trained_model_path = 'runs/detect/yolov11_pascal_voc_final/weights/best.pt'
 
 # === PATH TO IMAGE FOR INFERENCE ===
 
-test_image_path = r"C:\Users\Saud Masood Khan\Downloads\WhatsApp Image 2025-06-11 at 5.21.13 PM.jpeg"
+test_image_path = r"Image 2025-06-11 at 5.21.13 PM.jpeg" //add path to not have errors
 
 # === LOAD THE TRAINED MODEL ===
 model = YOLO(trained_model_path)
